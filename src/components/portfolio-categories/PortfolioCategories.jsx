1c2ee3a2d225084133eec8277aaf58e54a2a9f59
Lidiia Kalakutska <lidia.kalakutskaya@gmail.com>
2023-06-05 10:26:19 +0200

    Fixed spaces between sites, added clue about clickable sites.

--- src/components/portfolio-categories/PortfolioCategories.jsx
+++ src/components/portfolio-categories/PortfolioCategories.jsx
@@ -5,7 +5,7 @@ const PortfolioCategories = ({categories, selectedCategory, setSelectedCategory}
     return (
         <div className="works">
                 <div className="title"> my works </div>
-                <h2 className='subtitle'>Featured Works</h2>
+                <h2 className='subtitle'>Featured Works<span className='clue'>(clickable)</span></h2>
                 <div className="categories">
                     {categories.map(category => 
                         <div key={category} className={selectedCategory==category?"selected-category category":"category"} onClick={() => setSelectedCategory(category)}>{category}</div>
