7e239a7d6ce4e10353e3bea68b3d02c35ba07047
Lidiia Kalakutska <lidia.kalakutskaya@gmail.com>
2023-06-16 13:40:23 +0200

    Deleted timer

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -9,14 +9,14 @@ const About = () => {
         title: 'who am i',
         subtitle: 'about me',
         text: [
-            'Hi! I’m Lidia Kalakutska - front-end developer from Ukraine who has passion for building nice applications with intuitive functionality.',
+            'Hi! I’m Lidia Kalakutska - front-end developer from Ukraine who has passion for building nice and useful applications.',
             'Last year I worked as a Java developer in the largest outsourcing company in Ukraine, I developed features for simplifying managing financial operations for medical corporations.',
             'Also, I collaborated with third parties (payment systems), clarified interactions between international teams, contributed to code optimization, and helped new teammates get into the development.',
             'I’m always curious about learning new skills, tools, and concepts.',
             'I like visuals so I have done a front-end course, and created web-sites, programs, and games with new knowledges.',
             'This site is also one of these projects - it created on React with responsive design, you can try other my projects here, filter them by skills or categories.',
             <p>You can find my CV <a href='https://www.figma.com/proto/lke6xTQfWXHCTNP9JyPgzN/CV---Lidiia-Kalakutska---FrontEnd-developer-(React)?node-id=2-20' target='_blank'>here</a>.</p>,
-            `Now I am living in Czech Republic, looking for my dream job, and I would be happy if we match with each other.`,
+            `Now I am living in Czech Republic, looking for interesting remote job, and I would be happy if we match with each other.`,
         ]
     }
     return (
--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -5,7 +5,6 @@ import hotel from '../../img/sites/markup/hotel.png'
 import todo from '../../img/sites/react/todo.png'
 import comments from '../../img/sites/react/comments.png'
 import pacman from '../../img/sites/js/pacman.png'
-import timer from '../../img/sites/js/timer.png'
 import calc from '../../img/sites/js/calc.png'
 import PortfolioCategories from '../portfolio-categories/PortfolioCategories';
 import PortfolioSites from '../portfolio-sites/PortfolioSites';
@@ -24,7 +23,6 @@ const Portfolio = ({selectedSkill, resetSkill}) => {
         {alt:'Todo', href:'https://ariannaliss.github.io/mood-adviser/', git:'https://github.com/AriannaLiss/mood-adviser', src: todo, categories:[REACT], skills:[_REACT,_JS]}, 
         {alt:'Pacman', href:'http://ariannaliss.github.io/pacman/', git:'https://github.com/AriannaLiss/pacman', src: pacman, categories:[JS], skills:[_JS,_HTML,_CSS]},
         {alt:'Comments', href:'https://ariannaliss.github.io/ReactAPIComments/', git:'https://github.com/AriannaLiss/ReactAPIComments', src: comments, categories:[REACT], skills:[_REACT,_JS]}, 
-        {alt:'Timer', href:'http://ariannaliss.github.io/timer/', git:'https://github.com/AriannaLiss/timer', src: timer, categories:[JS], skills:[_JS]},
         {alt:'Hotel', href:'https://ariannaliss.github.io/hotel/', git:'https://github.com/AriannaLiss/hotel', src: hotel, categories:[MARKUP], skills:[_HTML,_SCSS]},
         {alt:'Calculator', href:'http://ariannaliss.github.io/calc/', git:'https://github.com/AriannaLiss/calc', src: calc, categories:[JS], skills:[_JS]},
     ]
